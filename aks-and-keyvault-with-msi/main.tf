5b91bb33af7e5368dd552d4e4892e6cc924708a9
Michael Elleby <michael@slapdogaf.dk>
2020-12-07 11:04:04 +0100

    Generate random name for keyvault to avoid name conflicts

--- aks-and-keyvault-with-msi/main.tf
+++ aks-and-keyvault-with-msi/main.tf
@@ -6,13 +6,23 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+resource "random_password" "secret" {
+  length = 16
+  special = true
+}
+
+resource "random_string" "keyvault_name" {
+  length = 20
+  override_special = "-"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group.name
   location = var.resource_group.location
 }
 
 resource "azurerm_key_vault" "vault" {
-  name = "kv-withaksandmsi"
+  name = random_string.keyvault_name.result
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   tenant_id = data.azurerm_client_config.current.tenant_id
